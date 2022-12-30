e7d19d397ba85fba5a3fb316c094cc1c36d0600d
kangharyeom <kangharyeom@gmail.com>
2022-12-31 02:43:05 +0900

    [Feat] LoginResponseDto 추가

--- back/src/main/java/stackoverflow/auth/handler/MemberAuthenticationSuccessHandler.java
+++ back/src/main/java/stackoverflow/auth/handler/MemberAuthenticationSuccessHandler.java
@@ -1,8 +1,12 @@
 package stackoverflow.auth.handler;
 
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import stackoverflow.auth.dto.LoginResponseDto;
+import stackoverflow.member.entity.Member;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,4 +21,16 @@ public class MemberAuthenticationSuccessHandler implements AuthenticationSuccess
         // 인증 성공 후, 로그를 기록하거나 사용자 정보를 response로 전송하는 등의 추가 작업을 할 수 있다.
         log.info("# Authenticated successfully!");
     }
+    private void sendSuccessResponse(HttpServletResponse response,
+                                     Authentication authentication) throws IOException {
+        Gson gson = new Gson();
+        Member member = (Member) authentication.getPrincipal();
+        LoginResponseDto dto = new LoginResponseDto();
+        dto.setMemberId(member.getMemberId());
+        dto.setPassword(member.getPassword());
+        dto.setEmail(member.getEmail());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().write(gson.toJson(dto, LoginResponseDto.class));
+    }
 }
+
