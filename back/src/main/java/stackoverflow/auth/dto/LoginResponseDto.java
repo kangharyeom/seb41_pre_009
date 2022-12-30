06b4bfab12cc1c393ecdccd4db6aa68c054af65c
kangharyeom <kangharyeom@gmail.com>
2022-12-31 02:41:45 +0900

    [Feat] LoginResponseDto 추가

---
+++ back/src/main/java/stackoverflow/auth/dto/LoginResponseDto.java
@@ -0,0 +1,12 @@
+package stackoverflow.auth.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginResponseDto {
+    private long memberId;
+    private String email;
+    private String password;
+}
